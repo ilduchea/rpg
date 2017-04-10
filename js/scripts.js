ae573504d7f63fcac7f47c4e4d7d76827de7fb16
Dominic Brown <yeahdom@gmail.com>
2017-04-10 16:50:58 -0700

    add conditions to properly check if game is ready to be played, add indicators to show the user which fields need to be updated

--- js/scripts.js
+++ js/scripts.js
@@ -129,62 +129,78 @@ $(document).ready(function() {
   var charImageCardChange = function() {
     let userClass = $(".char-class option:selected").val();
     let userGender = $(".char-gender option:selected").val();
-    if (userGender === "Male") {
-      switch (true) {
-        case (userClass === "Warrior"):
-          $(".char-img").attr('src', 'img/warrior_male1.png');
-          $(".combat #character").removeClass();
-          $(".combat #character").addClass("warrior_male1");
-          break;
-        case (userClass === "Mage"):
-          $(".char-img").attr('src', 'img/mage_male1.png');
-          $(".combat #character").removeClass();
-          $(".combat #character").addClass("mage_male1");
-          break;
-        case (userClass === "Ranger"):
-          $(".char-img").attr('src', 'img/ranger_male1.png');
-          $(".combat #character").removeClass();
-          $(".combat #character").addClass("ranger_male1");
-          break;
-        default:
-          console.log("switch default!");
-      }  // end switch
-    } else if (userGender === "Female") {
-      switch (true) {
-        case (userClass === "Warrior"):
-          $(".char-img").attr('src', 'img/warrior_fem1.png');
-          $(".combat #character").removeClass();
-          $(".combat #character").addClass("warrior_fem1");
-          break;
-        case (userClass === "Mage"):
-          $(".char-img").attr('src', 'img/mage_fem1.png');
-          $(".combat #character").removeClass();
-          $(".combat #character").addClass("mage_fem1");
-          break;
-        case (userClass === "Ranger"):
-          $(".char-img").attr('src', 'img/ranger_fem1.png');
-          $(".combat #character").removeClass();
-          $(".combat #character").addClass("ranger_fem1");
-          break;
-        default:
-          console.log("switch default!");
-      }  // end switch
+    if (userClass === "Select") {
+      $(".char-img").attr('src', 'img/arms1.png');
+      $(".combat #character").removeClass();
     } else {
-      console.log("gender neutral")
-    } // END IF statement
-  }
+      if (userGender === "Male") {
+        switch (true) {
+          case (userClass === "Warrior"):
+            $(".char-img").attr('src', 'img/warrior_male1.png');
+            $(".combat #character").removeClass();
+            $(".combat #character").addClass("warrior_male1");
+            break;
+          case (userClass === "Mage"):
+            $(".char-img").attr('src', 'img/mage_male1.png');
+            $(".combat #character").removeClass();
+            $(".combat #character").addClass("mage_male1");
+            break;
+          case (userClass === "Ranger"):
+            $(".char-img").attr('src', 'img/ranger_male1.png');
+            $(".combat #character").removeClass();
+            $(".combat #character").addClass("ranger_male1");
+            break;
+          default:
+            console.log("switch default!");
+        }  // end switch
+      } else if (userGender === "Female") {
+        switch (true) {
+          case (userClass === "Warrior"):
+            $(".char-img").attr('src', 'img/warrior_fem1.png');
+            $(".combat #character").removeClass();
+            $(".combat #character").addClass("warrior_fem1");
+            break;
+          case (userClass === "Mage"):
+            $(".char-img").attr('src', 'img/mage_fem1.png');
+            $(".combat #character").removeClass();
+            $(".combat #character").addClass("mage_fem1");
+            break;
+          case (userClass === "Ranger"):
+            $(".char-img").attr('src', 'img/ranger_fem1.png');
+            $(".combat #character").removeClass();
+            $(".combat #character").addClass("ranger_fem1");
+            break;
+          default:
+            console.log("switch default!");
+        }  // end switch
+      } else if (userGender === "Other") {
+        $(".char-img").attr('src', 'img/unicorn1.png');
+        $(".combat #character").removeClass();
+        $(".combat #character").addClass("unicorn1");
+      } else {
+          console.log("need to select")
+      } // END sub IF
+    } // END master IF
+  } // END charImageCardChange function
 
   var checkGameReady = function() {
     $("#no-name").text("");
     $("#no-roll").text("");
-    if (($("#input-name").val() === "") || (newGame.rollCount === 0)) {
+    $("#no-class").text("");
+    $("#no-gender").text("");
+    if (($("#input-name").val() === "") || (newGame.rollCount === 0) || ($(".char-class").val() === "Select") || ($(".char-gender").val() === "Select")) {
+      newGame.ready = false;
       if ($("#input-name").val() === "") {
-        newGame.ready = false;
         $("#no-name").text("<<<<");
       }
       if (newGame.rollCount === 0) {
-        newGame.ready = false;
         $("#no-roll").text("<<<<");
+      }
+      if ($(".char-class").val() === "Select") {
+        $("#no-class").text("<<<<");
+      }
+      if ($(".char-gender").val() === "Select") {
+        $("#no-gender").text("<<<<");
       }
     } else {
       newGame.ready = true;
@@ -194,10 +210,12 @@ $(document).ready(function() {
 
   // check if the CLASS dropdown was clicked and update the image
   $("select.char-class").change(function() {
+    $("#no-class").text("");
     charImageCardChange();
   });
   // check if the GENDER dropdown was clicked and update the image
   $("select.char-gender").change(function() {
+    $("#no-gender").text("");
     charImageCardChange();
   });
   // role the 8-sided die and update stats when die is clicked
@@ -208,6 +226,7 @@ $(document).ready(function() {
     $("#input-dex").val(dieRoll2to8());
     $("#input-int").val(dieRoll2to8());
     $("#input-con").val(dieRoll2to8());
+    $("#no-roll").text("");
   });
 
   $("#revealText").click(function(){
